f9ecdd4dc8ca16467bcc2d2e4d64e57b27642ee3
advjuniorpedrocolombo-arch <advjuniorpedrocolombo@gmail.com>
2026-09-20 10:08:31 -0300

    Configura plataforma PE - 1 MTEC Administracao

---
+++ config.js
@@ -0,0 +1,9 @@
+window.PORTAL_CONFIG={
+  apiUrl:"",
+  turma:"1º MTEC - Administração - Mairinque",
+  componente:"PE",
+  spreadsheetId:"1QpyTzbc2P0F-LrGw7fIe7m0MOE10rbVSFAyCiWrgCNI",
+  driveRootId:"1oUeynmQmDBTemiDZCXn-keIIw6Ga3TAA",
+  materiaisFolderId:"1lg2ToRMQ-XvvBCrBQehbZZdAqFIUbYpy",
+  entregasFolderId:"1tGnmfyLALLVaTTZwzoDc_oCCSJnAQZlZ"
+};
