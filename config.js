89d62a7bc19c3a3a550a3718e8a1edbaea04a3cb
advjuniorpedrocolombo-arch <advjuniorpedrocolombo@gmail.com>
2026-09-20 10:19:23 -0300

    Configura backend web app de PE

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.PORTAL_CONFIG={
-  apiUrl:"",
+  apiUrl:"https://script.google.com/macros/s/AKfycbw9HFmdoLRNxIkaMREEQERKeL9djSsgxFOpkodEa0EfdFue6AFz4D_TCMR-ljZRyWy8hA/exec",
   turma:"1º MTEC - Administração - Mairinque",
   componente:"PE",
   spreadsheetId:"1QpyTzbc2P0F-LrGw7fIe7m0MOE10rbVSFAyCiWrgCNI",
